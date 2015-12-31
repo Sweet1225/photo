059f675f9df0156fe2e17416e1f436db97d5f177
apple <1965888398@qq.com>
2015-12-31 10:27:13 +0800

    Initial Commit

---
+++ photo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  photo
+//
+//  Created by apple on 15/12/31.
+//  Copyright © 2015年 Sweet. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
